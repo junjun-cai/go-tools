5459f0f1af4e846646ebc073c1a250ffddab6ff7
蔡君君 <15229895912@163.com>
2022-01-24 10:30:25 +0800

    ReverseString

--- utils/str.go
+++ utils/str.go
@@ -69,3 +69,17 @@ func BKDRHash(str string) int {
 	}
 	return hash & 0x7FFFFFFF
 }
+
+// ***********************************************************************************************
+// * SUMMARY:
+// * WARNING:
+// * HISTORY:
+// *    -create: 2022/01/06 15:55:39 ColeCai.
+// ***********************************************************************************************
+func ReverseString(s string) string {
+	runes := []rune(s)
+	for from, to := 0, len(runes)-1; from < to; from, to = from+1, to-1 {
+		runes[from], runes[to] = runes[to], runes[from]
+	}
+	return string(runes)
+}
--- utils/str_test.go
+++ utils/str_test.go
@@ -43,3 +43,17 @@ func BenchmarkBKDRHash(b *testing.B) {
 		BKDRHash(StdStr)
 	}
 }
+
+//go test -bench=BenchmarkReverseString -benchmem
+//goos: windows
+//goarch: amd64
+//cpu: Intel(R) Core(TM) i5-10400 CPU @ 2.90GHz
+//BenchmarkReverseString-12        2747692               451.5 ns/op           256 B/op          2 allocs/op
+//BenchmarkReverseString-12        2775624               432.6 ns/op           256 B/op          2 allocs/op
+//BenchmarkReverseString-12        2763499               433.5 ns/op           256 B/op          2 allocs/op
+func BenchmarkReverseString(b *testing.B) {
+	s := "go test -bench=BenchmarkReverseString -benchmem"
+	for i := 0; i < b.N; i++ {
+		ReverseString(s)
+	}
+}
